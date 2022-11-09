cd364502e3080e79c824a5e40b87709b4b363edb
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-11-09 23:12:24 +0530

    Login and Signup Frontend page Added

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Login from './Login'
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<h1>Welcome</h1>}/>
+        {/* <Route path='/' element={<h1>Welcome</h1>}/> */}
+        <Route path='/' element={<Login/>}/>
     </Routes>
   )
 }
