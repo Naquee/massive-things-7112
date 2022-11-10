3be537ba944861fe7dda6bef5a26fe2be36ea9df
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-11-10 13:13:06 +0530

    Page Added

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -9,48 +9,50 @@ import footer from "./assets/footer.png"
 const Footer = () => {
   return (
     <>
-    <div style={{display:"flex",gap:"10rem",margin:"auto",width:"80%"}}>
-    <Box>
-    <p style={{color:"#84c225"}}>bigbasket</p>
-    <p>About Us</p>
-    <p>In News</p>
-    <p>Green bigbasket</p>
-    <p>Privacy Policy</p>
-    <p>Affiliate</p>
-    <p>Terms and Conditions</p>
-    <p>Careers At bigbasket</p>
-    <p>bb Instant</p>
-    <p>bb Daily</p>
-    <p>bb Blog</p>
-    <p>bbnow</p>
+      <Box sx={{ display: "flex", gap: { xs: '1rem', md: '8rem' }, margin: "auto", width: "80%", flexDirection: { xs: 'column', md: 'row' }, justifyContent: 'center', alignItems: { xs: 'center', md: 'stretch' } }}>
+        <Box sx={{ display: "flex", flexDirection: 'column', alignItems: 'flex-start', fontSize: 'small' }}>
+          <h3 style={{ color: "#84c225" }}>bigbasket</h3>
+          <p>About Us</p>
+          <p>In News</p>
+          <p>Green bigbasket</p>
+          <p>Privacy Policy</p>
+          <p>Affiliate</p>
+          <p>Terms and Conditions</p>
+          <p>Careers At bigbasket</p>
+          <p>bb Instant</p>
+          <p>bb Daily</p>
+          <p>bb Blog</p>
+          <p>bbnow</p>
 
-    </Box>
-    <Box>
-    <p style={{color:"#84c225"}}>Help</p>
-    <p>FAQs</p>
-    <p>Contact Us</p>
-    <p>bb Wallet FAQs</p>
-    <p>bb Wallet T&Cs</p>
-    <p>Vendor Connect</p>
-    </Box>
-    <Box>
-    <p style={{color:"#84c225"}}>Download Our App</p>
-    <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Google-App-store-icon.png" alt="" />
-    <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Apple-App-store-icon.png" alt="" />
-    </Box>
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: 'column', alignItems: 'flex-start', fontSize: 'small'}}>
+          <h3 style={{ color: "#84c225" }}>Help</h3>
+          <p>FAQs</p>
+          <p>Contact Us</p>
+          <p>bb Wallet FAQs</p>
+          <p>bb Wallet T&Cs</p>
+          <p>Vendor Connect</p>
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: 'column', alignItems: 'flex-start', fontSize: 'small' }}>
+          <h3 style={{ color: "#84c225" }}>Download Our App</h3>
+          <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Google-App-store-icon.png" alt="" />
+          <img src="https://www.bbassets.com/static/v2608/custPage/build/content/img/Apple-App-store-icon.png" alt="" />
+        </Box>
 
-    <Box>
-    <p style={{color:"#84c225"}}>Get Social With Us</p>
-   <div style={{gap:"20px",width: "300px"}}>
-   <span style={{color:"#3b5998"}}><FacebookIcon/></span>
-    <span style={{color:"#db3236"}}><PinterestIcon/></span>
-    <span style={{color:"#00aced"}}><TwitterIcon/></span>
-    <span style={{color:"#517fa4"}}><InstagramIcon/></span>
-   </div>
-    </Box>
-    </div>
+        <Box  >
+          <h3 style={{ color: "#84c225" }}>Get Social With Us</h3>
+          <div style={{ gap: "20px", width: "300px" }}>
+            <span style={{ color: "#3b5998" }}><FacebookIcon /></span>
+            <span style={{ color: "#db3236" }}><PinterestIcon /></span>
+            <span style={{ color: "#00aced" }}><TwitterIcon /></span>
+            <span style={{ color: "#517fa4" }}><InstagramIcon /></span>
+          </div>
+        </Box>
+      </Box>
 
-    <img src={footer} alt="footer" />
+      <Box sx={{ width:{md:"100%", xs:"50%", overflow:'hidden'}}}>
+        <img src={footer} alt="footer" />
+      </Box>
     </>
 
   )
--- frontend/src/Routes/VegAndFruits.jsx
+++ frontend/src/Routes/VegAndFruits.jsx
@@ -5,59 +5,56 @@ import Filter from "../Components/Filter";
 import VegCard from "../Components/vegCard";
 import { getVeg } from "../Redux/App/action";
 
-
-
-const Vegs = () => {
+const VegsAndFruits = () => {
 
     const vegs = useSelector(state => state.AppReducer.veg);
     const [searchParams] = useSearchParams();
-
     const dispatch = useDispatch();
     const location = useLocation();
 
     useEffect(() => {
-        if(location || vegs.length === 0){
+        if (location || vegs.length === 0) {
             let getVegsParams = {
-                params : {
-                    category : searchParams.getAll('category'),
+                params: {
+                    category: searchParams.getAll('category'),
                 }
             }
 
             dispatch(getVeg(getVegsParams))
         }
     }, [location.search])
-   
+
     console.log(vegs)
 
     return (
-        <div style={{display:"flex",
-       gap:"2rem",
-       textAlign:"left"
-      
-    }}>
-      <div style={{width:"200px", border:"2px solid black", paddingLeft:"1rem"}}>
-      <Filter/>
-      </div>
-      <div style={{
-        border:'1px solid black',
-         display: 'grid',
+        <div style={{
+            display: "flex",
+            gap: "2rem",
+            textAlign: "left"
 
-         gridTemplateColumns:'repeat(4, 1fr)' ,
-         gridGap:"2rem",
-         
-         
-      }}>
-        { vegs.length > 0 && vegs.map((veges) => {
-             return <div key={veges._id}>
-             
-                 <VegCard vegId={veges}/>
-                
-             </div> 
-        }) }
-      </div>
-    </div>
+        }}>
+            <div style={{ width: "200px", border: "2px solid black", paddingLeft: "1rem" }}>
+                <Filter />
+            </div>
+            <div style={{
+                border: '1px solid black',
+                display: 'grid',
+
+                gridTemplateColumns: 'repeat(4, 1fr)',
+                gridGap: "2rem",
+
+
+            }}>
+                {/* {vegs.length > 0 && vegs.map((veges) => {
+                    return <div key={veges._id}>
+
+                        <VegCard vegId={veges} />
+
+                    </div>
+                })} */}
+            </div>
+        </div>
     );
 }
 
-
-export default Vegs;
+export default VegsAndFruits;
