9a489995473eb66c43aaf066cc8d714b8bf10663
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-11-12 21:44:08 +0530

    admin pannel Added

--- backend/Routes/Data.Routes.js
+++ backend/Routes/Data.Routes.js
@@ -4,13 +4,17 @@ const Data = Router();
 
 Data.get("/products", async (req, res) => {
     const { _sort, _order, country } = req.query;
-    console.log(req.query)
     let sortData = {}
     sortData[_sort] = _order === 'desc' ? -1 : 1;
 
     if (country || sortData) {
-        const data = await ProductModel.find({country: country}).sort(sortData);
-        res.send(data)
+        if (sortData  && country) {
+            const data = await ProductModel.find({ country: country }).sort(sortData);
+            res.send(data)
+        } else {
+            const data = await ProductModel.find().sort(sortData);
+            res.send(data)
+        }
     } else {
         const data = await ProductModel.find();
         res.send(data)
